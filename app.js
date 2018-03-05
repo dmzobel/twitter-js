16a6c08fb31cb594b1ded3e3253500f5acf74ca6
Anurag Prasad <anuyorker@gmail.com>
2018-03-05 17:53:17 -0500

    started server

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const app = express(); // create instance of express application
 
+app.get('/', (req, res) => res.send('Welcome!'));
+
+app.get('/news', (req, res) => res.send('News Page'));
+
+app.listen(3000, () => console.log('App listening on port 3000!'));
