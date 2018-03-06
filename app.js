6dfc6402c1d00d4807aa4bc91e99db883e210a9d
Anurag Prasad <anuyorker@gmail.com>
2018-03-06 11:23:57 -0500

    rendered template

--- app.js
+++ app.js
@@ -2,27 +2,9 @@ const express = require('express');
 const app = express(); // create instance of express application
 const nunjucks = require('nunjucks');
 
-// Log middleware
-app.use(function (req, res, next) {
-  console.log(req.method, req.url, res.statusCode);
-  // call `next`, or else your app will be a black hole — receiving requests but never properly responding
-  next();
-});
-
-app.use('/news', function (req, res, next) {
-  console.log(`You've reached the news page`);
-  next();
-});
-
-app.get('/', (req, res) => res.send('Welcome!'));
-
-app.get('/news', (req, res) => res.send('News Page'));
-
-app.listen(3000, () => console.log('App listening on port 3000!'));
-
 // Nunjucks
 var locals = {
-  title: 'Example Title',
+  title: 'Hall of Fame',
   people: [
     { name: 'Gandalf' },
     { name: 'Frodo' },
@@ -39,3 +21,23 @@ nunjucks.render('index.html', locals, function (err, output) {
   console.log(output);
 });
 
+// Log middleware
+app.use(function (req, res, next) {
+  console.log(req.method, req.url, res.statusCode);
+  // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+  next();
+});
+
+app.use('/news', function (req, res, next) {
+  console.log(`You've reached the news page`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  // Render Template
+  res.render('index', locals);
+});
+
+app.get('/news', (req, res) => res.send('News Page'));
+
+app.listen(3000, () => console.log('App listening on port 3000!'));
