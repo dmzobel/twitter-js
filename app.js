c336d5ffacc93f177504870981d1b452cab2f827
Marshall Zobel <dmzobel@gmail.com>
2018-03-06 12:36:42 -0500

    updated html, css, and index.js files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express(); // create instance of express application
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
 
 // Nunjucks
 var locals = {
@@ -26,6 +27,8 @@ nunjucks.render('index.html', locals, function (err, output) {
 });
 
 // Log middleware
+app.use('/', routes);
+
 app.use(function (req, res, next) {
   console.log(req.method, req.url, res.statusCode);
   // call `next`, or else your app will be a black hole — receiving requests but never properly responding
@@ -36,12 +39,5 @@ app.use('/news', function (req, res, next) {
   console.log(`You've reached the news page`);
   next();
 });
-
-app.get('/', (req, res) => {
-  // Render Template
-  res.render('index', locals);
-});
-
-app.get('/news', (req, res) => res.send('News Page'));
 
 app.listen(3000, () => console.log('listening on port 3000'));
