8ceb97963f7fa792533d18c6f83cb15daefcc0a7
Anurag Prasad <anuyorker@gmail.com>
2018-03-06 10:33:44 -0500

    logged middleware

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express(); // create instance of express application
 
+// Log middleware
+app.use(function (req, res, next) {
+  console.log(req.method, req.url, res.statusCode);
+  // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+  next();
+});
+
+app.use('/news', function (req, res, next) {
+  console.log(`You've reached the news page`);
+  next();
+});
+
+
 app.get('/', (req, res) => res.send('Welcome!'));
 
 app.get('/news', (req, res) => res.send('News Page'));
